9346babdb2360a5c7b2816f4bd73712658cf6c6d
atsushi <green515tea@gmail.com>
2023-08-30 23:02:07 +0900

    [add]ある駅に近しい駅を出力する機能を追加

--- scripts/vanilla_auto_encoder.py
+++ scripts/vanilla_auto_encoder.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 import torch
 from GCN import GCN
 from PathSetting import PathSetting
-from pre_post_process import draw_feature, make_station_dataframe
+from pre_post_process import draw_feature, make_station_dataframe, save_features
 from StationData import StationData
 
 FONTNAME = "IPAexGothic"
 plt.rcParams["font.family"] = FONTNAME
-HIDDIN_DIM_LIST: final = [2]
+HIDDIN_DIM_LIST: final = [20, 20, 20]
 WEIGHT_DECAY: final = 5e-4
-LR = 0.1
-DROP_OUT = 0.1
+LR: final = 0.001
+DROP_OUT: final = 0.1
 
 
 def main():
@@ -46,19 +46,15 @@ def main():
             f"Train loss: {train_loss:.4f}, " f"Val loss: {val_loss:.4f}, " f"Test loss: {test_loss:.4f}",
         )
     embs = model.get_embeddings(data.x, data.edge_index)
-    print(embs)
-    print(embs.shape)
+
     draw_feature(embs.detach().cpu().numpy(), station_df["駅名"].values, station_df["地価"].values)
+    save_features(ps, embs.detach().cpu().numpy(), station_df["駅名"].values)
 
 
 def train(model, data, optimizer, loss_function):
     model.train()
     optimizer.zero_grad()
     out = model(data.x, data.edge_index)
-    print("out", out[data.train_mask][0])
-    print("y", data.y[data.train_mask][0])
-    print()
-    print()
     loss = loss_function(out[data.train_mask], data.y[data.train_mask])
     loss.backward()
     optimizer.step()
