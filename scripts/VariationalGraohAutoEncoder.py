e9a6e830d9936e3b894278e8e31611b0900898e8
atsushi <green515tea@gmail.com>
2023-09-07 19:56:32 +0900

    [update]エッジ予測誤差を追加

--- scripts/VariationalGraohAutoEncoder.py
+++ scripts/VariationalGraohAutoEncoder.py
@@ -7,50 +7,9 @@ from torch_geometric.nn import GCNConv, SAGEConv
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-# ショートカットコネクションを入れたほうがいいかもしれないが、オートエンコーダーの性質上、
-# ショートカットコネクションを入れると予測が簡単になりすぎてしまうのではないかという懸念がある。
-
 # Attentionも試したが、学習に時間がかかる上に、精度も悪かったので今回はSAGEConvを使うことにした。
 # SAGEConv epoch:100000, loss:2.9652
 # GATConv epoch:100000, loss:3.3080
-
-
-def initial_residual(x: torch.Tensor, h: torch.Tensor) -> torch.Tensor:
-    """skip connectionを入れるための関数
-
-    Args:
-        x (torch.Tensor): 前の層の出力
-        h (torch.Tensor): 今の層の出力
-
-    Returns:
-        torch.Tensor: skip connectionを入れた後の出力
-    """
-    x = fit_tensor_size(x=x, out_dim=h.shape[1])
-    return x + h
-
-
-def fit_tensor_size(x: torch.Tensor, out_dim: int) -> Tuple[torch.Tensor, torch.Tensor]:
-    """xの各ノードが持つベクトルの次元数をout_dimに合わせる。
-    xの方が大きい場合は切り取り、小さい場合は0でパディングする。
-
-    Args:
-        x (torch.Tensor): 形を合わせる前のtensor
-        out_dim (int): xの2軸目の次元数をこの次元数に合わせる
-
-    Returns:
-        Tuple[torch.Tensor, torch.Tensor]: 形を合わせた後のtensor
-    """
-    if x.shape[1] < out_dim:
-        # 前の層の出力の次元数の方が小さい時、前の層の出力を0でパディングする
-        x = torch.cat([x, torch.zeros((x.shape[0], out_dim - x.shape[1]), dtype=torch.float).to(device)], dim=1)
-    elif x.shape[0] > out_dim:
-        # 前の層の出力の次元数の方が大きい時、前の層の出力を削って次元数を削る
-        x = x[:, :out_dim]
-    else:
-        # 同じ場合はそのまま
-        pass
-    return x
 
 
 class VariationalGraohAutoEncoder(torch.nn.Module):
@@ -71,14 +30,12 @@ class VariationalGraohAutoEncoder(torch.nn.Module):
 
     def forward(self, x: torch.Tensor, edge_index: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         h = self.conv1(x, edge_index).relu()
-        h = initial_residual(x, h)
         for conv, res in zip(self.conv_list, self.initial_residual_list):
             # graph conv
             h = conv(h, edge_index).relu()
             # initial residual
             x_ = res(x)
             h = h + x_
-            # h = initial_residual(x, h)
         return self.conv_mu(h, edge_index), self.conv_logstd(h, edge_index)
 
 
@@ -86,6 +43,7 @@ class VariationalGraohAutoDecoder(torch.nn.Module):
     def __init__(self, embedding_channels: int, hidden_channels_list: List[int], out_channels: int):
         super().__init__()
         self.sigmoid = nn.Sigmoid()
+        # 自己符号化器としてのデコーダー
         self.conv1 = SAGEConv(embedding_channels, hidden_channels_list[0])
         self.conv_list = [
             SAGEConv(hidden_channels_list[i], hidden_channels_list[i + 1]) for i in range(len(hidden_channels_list) - 1)
@@ -98,9 +56,20 @@ class VariationalGraohAutoDecoder(torch.nn.Module):
         # 最終層だけGCNConvで隣接ノードを全て使う
         self.conv_final = GCNConv(hidden_channels_list[-1], out_channels)
 
+        # エッジ予測としてのデコーダー
+        # エッジ予測に InnerProductDecoder を使うと、ベクトルが似たノード同士でエッジができやすくなるので、
+        # エッジ予測はニューラルネットワークで行う。
+        first_layer = nn.Linear(embedding_channels * 2, hidden_channels_list[0])  # 2つのノードの埋め込みをconcatしている
+        mid_layer = [
+            nn.Linear(hidden_channels_list[i], hidden_channels_list[i + 1])
+            for i in range(len(hidden_channels_list) - 1)
+        ]
+        self.edge_predict_linear = [first_layer] + mid_layer
+        self.edge_predict_final = nn.Linear(hidden_channels_list[-1], 1)
+
     def forward(self, z: torch.Tensor, edge_index: torch.Tensor) -> torch.Tensor:
+        # 自己符号化器としてのデコーダー
         h = self.conv1(z, edge_index).relu()
-        h = initial_residual(z, h)
         for conv, res in zip(self.conv_list, self.initial_residual_list):
             # graph conv
             h = conv(h, edge_index).relu()
@@ -114,3 +83,20 @@ class VariationalGraohAutoDecoder(torch.nn.Module):
         # 0-1をとる変数にはシグモイド関数をかませる
         h[:, CROSS_ENTROPY_INDEXES] = self.sigmoid(h[:, CROSS_ENTROPY_INDEXES])
         return h
+
+    def edge_pred_forward(self, z1: torch.Tensor, z2: torch.Tensor) -> torch.Tensor:
+        """z1の埋め込みを持つノードと、z2の埋め込みを持つノードの間にエッジがある確率を返す。
+
+        Args:
+            z1 (torch.Tensor): 埋め込みベクトル
+            z2 (torch.Tensor): 埋め込みベクトル
+
+        Returns:
+            torch.Tensor: エッジの有無を表す確率
+        """
+        # エッジ予測としてのデコーダー
+        h = torch.cat([z1, z2], dim=1)
+        for linear in self.edge_predict_linear:
+            h = linear(h).relu()
+        h = self.edge_predict_final(h)
+        return self.sigmoid(h)
